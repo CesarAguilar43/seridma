5f48361f7d058052eca4d9683bec9d45c95d6244
Cesar <cesaraguilar196@gmail.com>
2021-05-21 22:49:44 -0500

    Cambios en general de la app

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -2,81 +2,39 @@ import { Carousel } from 'react-bootstrap';
 import image from '../../assets/img/circuitos.jpeg';
 import image2 from '../../assets/img/pexels-pixabay-163100.jpg';
 import image3 from '../../assets/img/pexels-pixabay-257736.jpg';
-//import '../../assets/css/Carousel.css';
+import '../../assets/css/Carousel.css';
 
 
 const CarouselHome = () => {
     return (
         <Carousel fade>
-            <Carousel.Item interval={3000} style={{
-                height: '32rem',
-                background: '#000',
-                color: 'white',
-            }}>
+            <Carousel.Item interval={3000} className="carousel-item">
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 carousel-img"
                     src={image}
                     alt="First slide"
-                    style={{
-                        position: 'absolute',
-                        bottom: '0',
-                        left: '0',
-                        right: '0',
-                        top: '0',
-                        backgroundPosition: 'center',
-                        backgroundSize: 'cover',
-                        opacity: '0.5',
-                    }}
                 />
                 <Carousel.Caption>
                     <h3>¿Quienes somos?</h3>
                     <p>Somos una empresa 100% mexicana dedicada a brindar servicios integrales </p>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={3000} style={{
-                height: '32rem',
-                background: '#000',
-                color: 'white',
-            }}>
+            <Carousel.Item interval={3000} className="carousel-item">
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 carousel-img"
                     src={image2}
                     alt="Second slide"
-                    style={{
-                        position: 'absolute',
-                        bottom: '0',
-                        left: '0',
-                        right: '0',
-                        top: '0',
-                        backgroundPosition: 'center',
-                        backgroundSize: 'cover',
-                        opacity: '0.5',
-                    }}
                 />
                 <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={3000} style={{
-                height: '32rem',
-                background: '#000',
-                color: 'white',
-            }}>
+            <Carousel.Item interval={3000} className="carousel-item">
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 carousel-img"
                     src={image3}
                     alt="Third slide"
-                    style={{
-                        position: 'absolute',
-                        bottom: '0',
-                        left: '0',
-                        right: '0',
-                        top: '0',
-                        backgroundPosition: 'center',
-                        backgroundSize: 'cover',
-                        opacity: '0.5',
-                    }}
                 />
                 <Carousel.Caption>
                     <h3>Third slide label</h3>
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,16 +1,19 @@
 import React, { Fragment } from 'react';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
+import '../assets/css/Layout.css';
 
 function Layout(props) {
     return (
         <Fragment>
-            <div style={{backgroundColor: '#595959', color: '#ffffff'}}>Parte para numero de telefono, y plugins para enviar a fb u otra red social</div>
-            <Header />
-            <main>
-                {props.children}
-            </main>     
-            <Footer />
+            <div className="container-full">
+                <div style={{ backgroundColor: '#ccc', color: 'black' }}>Parte para numero de telefono, y plugins para enviar a fb u otra red social</div>
+                <Header />
+                <main>
+                    {props.children}
+                </main>
+                <Footer />
+            </div>
         </Fragment>
     );
 }
