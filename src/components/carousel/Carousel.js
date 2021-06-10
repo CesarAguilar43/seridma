888d50ca33c58d3e711aa22119f0c4409fa397e1
Cesar <cesaraguilar196@gmail.com>
2021-06-09 21:03:34 -0500

    Cambios en el carrusel y en el front de la app

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,44 +1,50 @@
 import { Carousel } from 'react-bootstrap';
-import image from '../../assets/img/circuitos.jpeg';
+import image from '../../assets/img/homeCarrusel.png';
+import image1 from '../../assets/img/empresa-limpieza-oficinas.jpg';
 import image2 from '../../assets/img/pexels-pixabay-163100.jpg';
-import image3 from '../../assets/img/pexels-pixabay-257736.jpg';
 import '../../assets/css/Carousel.css';
 
 
 const CarouselHome = () => {
     return (
         <Carousel className="carousel-full">
-            <Carousel.Item interval={3000} className="carousel-item">
+            <Carousel.Item interval={5000} className="carousel-item">
                 <img
                     className="d-block w-100 carousel-img"
                     src={image}
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>¿Quienes somos?</h3>
-                    <p>Somos una empresa 100% mexicana dedicada a brindar servicios integrales </p>
+                    <h1>¿QUIENES SOMOS NOSOTROS?</h1>
+                    <h3>
+                    Somos una empresa 100% mexicana, dedicada a brindar servicios 
+                    integrales de mantenimiento, distinguidos por nuestra calidad 
+                    y buenas practicas al realizar nuestras labores.
+                    </h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={5000} className="carousel-item">
+                <img
+                    className="d-block w-100 carousel-img"
+                    src={image1}
+                    alt="Second slide"
+                />
+                <Carousel.Caption>
+                    <h1>SERVICIO DE LIMPIEZA Y DESINFECCIÓN</h1>
+                    <h3>Para el hogar, comercio, industria u oficina 100% deducible de impuestos, descubre
+                        los beneficios que SERIDMA tiene para ti.
+                    </h3>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={3000} className="carousel-item">
                 <img
                     className="d-block w-100 carousel-img"
                     src={image2}
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={3000} className="carousel-item">
-                <img
-                    className="d-block w-100 carousel-img"
-                    src={image3}
                     alt="Third slide"
                 />
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h1>Third slide label</h1>
+                    <h3>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</h3>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
