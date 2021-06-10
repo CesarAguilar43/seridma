6ab3ae12734b1b3b5d36cc9a2294948968ece03b
Cesar <cesaraguilar196@gmail.com>
2021-06-09 21:19:34 -0500

    errores de fusion

--- src/containers/services/Services.js
+++ src/containers/services/Services.js
@@ -4,8 +4,17 @@ import Layout from '../../layouts/Layout';
 function Services() {
     return (
         <Layout>
-            <div>
-                <p>Esto sera el servicio de la pagina</p>
+            <div id="Services">
+                <h1>Portafolio de Servicios</h1>
+                <h2>Servicios electricos insdustriales</h2>
+                <ul>
+                    <li>Mantenimiento y repración a motores electricos.</li>
+                    <li>Bobinado de motores electricos.</li>
+                    <li>Canalización de todo tipo, cableado y conexión de fuerza.</li>
+                    <li>Mantenimiento preventivo a tableros de fuerza y control de CCM</li>
+                    <li>Montaje y desmontaje de equipo electrico (tableros de fuerza, motores electricos, bombas etc).</li>
+                    <li></li>
+                </ul>
             </div>
         </Layout>
     );
