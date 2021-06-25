a09d92f6ecca1436b54c2a1a3dc85f1c3d4a02d7
Hector Nuñez <arinnunez@gmail.com>
2021-06-25 11:19:17 -0500

    elimine imagenes que no ocupo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from '../../assets/img/Logo.png';
-import { Container, Row, Col,Image } from 'react-bootstrap';
+import logo from '../../assets/img/LOGO_SERIDMA_HD.png';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 const Footer = () => {
     return (
         <footer>
             <Container fluid>
-                <Row className = "justify-content-md-center" id="footer">
-                    <Col xs lg="3" id="col-top"><Image src={logo} width="150px" height="150px" rounded /></Col>
-                    <Col xs lg="3"><p>Contacto : <br/>
-                        Cel. (833) 153 9414 <br/>
+                <Row id="footer">
+
+                    <Col xs lg="3"><p>Contacto : <br />
+                        Cel. (833) 153 9414 <br />
                         Tel. (833) 399 4047
-                        <br/>
+                        <br />
                     </p>
 
-                    <p>  Correo : <br/>
-                        <a href="seridma.servicios@gmail.com">seridma.servicios@gmail.com</a></p>
+                        <p>  Correo : <br />
+                            <a href="seridma.servicios@gmail.com">seridma.servicios@gmail.com</a></p>
                     </Col>
+
                     <Col xs lg="2" className="redes-container"><ul>
                         <li><a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a></li>
                         <li><a href="https://www.youtube.com/channel/UCbS4aaMATiMPm4Rhvimj1BA" class="youtube"><i class="fab fa-youtube"></i></a></li>
-                        </ul></Col>
+                    </ul></Col>
+
+                    <Col xs lg="3" id="col-top"><Image src={logo} rounded /></Col>
                 </Row>
             </Container>
         </footer>
