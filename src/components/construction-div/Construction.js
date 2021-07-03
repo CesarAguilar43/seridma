7b55fee1a8ce7695571ae1695401b437a55adadb
Cesar <cesaraguilar196@gmail.com>
2021-07-03 16:14:52 -0500

    Cambios en el front

--- src/components/construction-div/Construction.js
+++ src/components/construction-div/Construction.js
@@ -1,15 +1,25 @@
 import construction from '../../assets/img/construccion.png';
-import { Image, Container } from 'react-bootstrap';
+import { Image, Container, Row, Col } from 'react-bootstrap';
 
 const Construction = () => {
     return (
         <Container fluid className="container-wrapper">
-            <Image src={construction} alt="En construcción" style={{ display: 'block', margin: '0 auto', }} />
-            <hr />
-            <h3 style={{ textAlign: 'center', margin: '0 0' }}>ESTAMOS EN CONSTRUCCIÓN PARA TI...</h3>
-            <h5 style={{ textAlign: 'center', margin: '0 0' }}>GRACIAS POR TU PREFERENCIA</h5>
-            <br /><br />
-        </Container>
+            <Row>
+                <Col xs={12} lg={12}>
+                    <div style={{ width: '350px', height: 'auto', margin: '0 auto'}}>
+                        <Image src={construction} alt="En construcción" style={{width: '100%' }} />
+                    </div>
+                </Col>
+                <Col xs={12} lg={12}>
+                    <hr />
+                </Col>
+                <Col xs={12} lg={12}>
+                    <h3 style={{ textAlign: 'center', margin: '0 0' }}>ESTAMOS EN CONSTRUCCIÓN PARA TI...</h3>
+                    <h5 style={{ textAlign: 'center', margin: '0 0' }}>GRACIAS POR TU PREFERENCIA</h5>
+                    <br /><br />
+                </Col>
+            </Row>
+        </Container >
     );
 }
 
